9fb4a47a15764e8641560a60b42ff34af928f416
OscarEd08 <oscar.pairazaman2@unmsm.edu.pe>
2021-12-22 14:40:55 -0500

    Documentando el codigo

--- Main.py
+++ Main.py
@@ -1,20 +1,26 @@
 import pandas as pd
 from pandas import ExcelWriter
 
+#Leemos los archivos excel como data frames
 file1 = pd.read_excel('Files/Archivo1.xlsx', header = None)
 file2 = pd.read_excel('Files/Archivo2.xlsx', header = None)
+
+#Creamos data frames vacíos
 file3 = pd.DataFrame()
 file4 = pd.DataFrame()
 
+#Hallamos el minimo número de filas de los archivos
 min = min(len(file1.index), len(file2.index))
 
+#Comparamos la igualdad de las celdas en minúsculas
 for i in range(min):
-    if((file1[0][i]).lower() == (file2 [0][i]).lower()):
+    if((file1[0][i]).lower() == (file2[0][i]).lower()):
         sameFiles = True
     else:
         sameFiles = False
 
 if(not(file1.equals(file2) and sameFiles)):
+    #Se crea el archivo 3
     for i in range(min):
         if((file1[0][i]).lower() != (file2 [0][i]).lower()):                                
             writer1 = ExcelWriter('Files/Archivo3.xlsx')
@@ -26,7 +32,8 @@ if(not(file1.equals(file2) and sameFiles)):
         print("No hay diferencias en las primeras", min, "filas")
     else:
         print(file3)
-          
+
+    #Se crea el archivo 4     
     if(len(file2.index) > len(file1.index)):
         writer2 = ExcelWriter('Files/Archivo4.xlsx')
         for j in range(len(file1.index), len(file2.index)):
